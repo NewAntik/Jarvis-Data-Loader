077f964e6e2168cf61a39a8c687fee013cf363f1
Antik <120013201+NewAntik@users.noreply.github.com>
2024-10-10 10:20:21 +0300

    Init

---
+++ src/main/java/ua/jarvis/data/loader/JarvisDataLoaderApplication.java
@@ -0,0 +1,11 @@
+package ua.jarvis.data.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisDataLoaderApplication {
+	public static void main(final String[] args) {
+		SpringApplication.run(JarvisDataLoaderApplication.class, args);
+	}
+}
